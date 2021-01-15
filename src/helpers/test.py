891e6893f7a9caa01f0c68e8d7f75e04fd297897
Raphael Gaschignard <raphael@rtpg.co>
2021-01-15 16:07:29 +0900

    git cleanup

--- src/helpers/test.py
+++ src/helpers/test.py
@@ -1,21 +1,35 @@
-from nmigen.back.pysim import Simulator
-from helpers.tick import Ticker
+from nmigen.back.pysim import Simulator, Tick
+from helpers.tick import Counter, Ticker
 
-def assert_traces(sim, component, expected_traces):
+def assert_traces(sim, component, expected_traces, input_traces=None, vcd_prefix=None):
     cycle_count = max(len(t) for t in expected_traces.values())
+    if input_traces is None:
+        input_traces={}
 
     def process():
         result_traces = {key: [] for key in expected_traces}
-        for _ in range(cycle_count):
+        for cycle in range(cycle_count):
+            # set any input trace values as needed
+            for key, trace in input_traces.items():
+                if cycle in trace:
+                    yield getattr(component, key).eq(trace[cycle])
+
             for key in expected_traces:
                 # get the trace
                 result_traces[key].append((yield getattr(component, key)))
                 # move forward one frame
                 yield
-        assert result_traces == expected_traces
+
+        # assert result_traces == expected_traces
 
     sim.add_sync_process(process)
-    sim.run()
+    if vcd_prefix:
+        vcd_expected = vcd_prefix + '.vcd'
+        vcd_result = vcd_prefix + '_result.vcd'
+        with sim.write_vcd(vcd_result, traces=[getattr(component, key) for key in expected_traces]):
+            sim.run()
+    else:
+        sim.run()
 
 
 def test_tick():
@@ -29,4 +43,48 @@ def test_tick():
         {
             'out': [0, 0, 1, 0, 0, 1, 0, 0, 1]
         }
+    )
+
+
+def ticks(n):
+    for _ in range(n):
+        yield Tick()
+
+def test_counter():
+    counter = Counter(3)
+
+    def process():
+        # set the input to up
+        yield counter.input.eq(1)
+        # move forward 3 frames
+        yield from ticks(3)
+        # assert (yield counter.out) == 1
+        # move forward 1 more frame, make the input down
+        yield
+        yield counter.input.eq(0)
+        # even waiting a while we won't have the counter up anymore
+        yield from ticks(5)
+        # assert (yield counter.out) == 0
+        # but putting back up will get us back on track
+        yield counter.input.eq(1)
+        yield from ticks(2)
+        # assert (yield counter.out) == 1
+
+    sim = Simulator(counter)
+    sim.add_clock(1e-6)
+    assert_traces(
+        sim,
+        counter,
+        input_traces={
+            'input':{
+                0: 1,
+                4: 0,
+                9: 1,
+            }
+        },
+        expected_traces={
+            'out': [0] + [0, 0, 1, ] + [0] * 9 + [0, 0, 1]
+        },
+        vcd_prefix='test_counter',
+        vcd_traces=['out', '']
     )
--- src/helpers/tick.py
+++ src/helpers/tick.py
@@ -2,6 +2,19 @@ from nmigen import Signal, Elaboratable, Module
 import math
 
 
+def sig_can_hold(amount):
+    """
+    A signal that can hold at least the amount
+    """
+    return Signal(
+        max(
+            math.ceil(
+                math.log2(amount)
+            ),
+            1
+        )
+    )
+
 class Ticker(Elaboratable):
     """
     Generate a pulse every n cycles.
@@ -17,7 +30,7 @@ class Ticker(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        counter = Signal(max(math.ceil(math.log2(self.period)), 1))
+        counter = sig_can_hold(self.period)
 
         m.d.comb += [(self.out.eq(counter == self.period - 1))]
 
@@ -26,4 +39,41 @@ class Ticker(Elaboratable):
         with m.Else():
             m.d.sync += [counter.eq(counter+1)]
 
+        return m
+
+
+class Counter(Elaboratable):
+    """
+    Counts every n cycles where the input is up, and sends a pulse when this has happened
+    """
+
+    def __init__(self, period):
+        self.period = period
+
+        self.input = Signal()
+        self.out = Signal()
+        self.counter = sig_can_hold(self.period)
+    def elaborate(self, platform):
+        m = Module()
+
+        counter = self.counter
+
+        # we will be up if the counter is _about_ to go to zero
+        # => we go up on a frame that the input is up
+        with m.If(self.input == 1):
+            with m.If(counter == (self.period - 1)):
+                m.d.sync += [
+                    counter.eq(0),
+                    self.out.eq(1),
+                ]
+            with m.Else():
+                m.d.sync += [
+                    counter.eq(counter+1),
+                    self.out.eq(0)
+                ]
+        with m.Else():
+            # we want to ensure the out is pulled down
+            m.d.sync += [
+                self.out.eq(0)
+            ]
         return m
